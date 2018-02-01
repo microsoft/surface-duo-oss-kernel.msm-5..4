0384e1f8c68aedcdf9dc23a0301ea3f40e96e694
Connor O'Brien <connoro@google.com>
2018-01-31 18:11:57 -0800

    ANDROID: cpufreq: track per-task time in state
    Add time in state data to task structs, and create
    /proc/<pid>/time_in_state files to show how long each individual task
    has run at each frequency.
    Create a CONFIG_CPU_FREQ_TIMES option to enable/disable this tracking.
    
    Bug: 72339335
    Bug: 127641090
    Test: Read /proc/<pid>/time_in_state
    Change-Id: Ia6456754f4cb1e83b2bc35efa8fbe9f8696febc8
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [adelva: Folded the following changes into this patch:
             a6d3de6a7fba ("ANDROID: Reduce use of #ifdef CONFIG_CPU_FREQ_TIMES")
             b89ada5d9c09 ("ANDROID: Fix massive cpufreq_times memory leaks")]
    Signed-off-by: Alistair Delva <adelva@google.com>
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- kernel/sched/cputime.c
+++ kernel/sched/cputime.c
@@ -2,6 +2,7 @@
 /*
  * Simple CPU accounting cgroup controller
  */
+#include <linux/cpufreq_times.h>
 #include "sched.h"
 #include <trace/hooks/sched.h>
 
@@ -134,6 +135,9 @@ void account_user_time(struct task_struct *p, u64 cputime)
 
 	/* Account for user time used */
 	acct_account_cputime(p);
+
+	/* Account power usage for user time */
+	cpufreq_acct_update_power(p, cputime);
 }
 
 /*
@@ -178,6 +182,9 @@ void account_system_index_time(struct task_struct *p,
 
 	/* Account for system time used */
 	acct_account_cputime(p);
+
+	/* Account power usage for system time */
+	cpufreq_acct_update_power(p, cputime);
 }
 
 /*
