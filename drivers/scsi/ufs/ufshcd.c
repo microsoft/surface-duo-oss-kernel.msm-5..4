23fb5ac4d3b03e8d4f91d4dc05b77e92dc5b5610
Can Guo <cang@codeaurora.org>
2019-12-05 18:16:12 -0800

    FROMLIST: scsi: ufs: Export query request interfaces
    Vendor drivers may need to send query requests in proprietary
    implementations. Hence, export the query request interfaces
    so that vendor drivers can use them.
    
    Removed export of internal helper functions:
    ufshcd_query_attr_retry and ufshcd_query_flag_retry
    
    Bug: 146013342
    Change-Id: Ibe3082097554e89e03b33a926ef013503bac0e74
    Link: https://lore.kernel.org/patchwork/patch/1163542/
    Signed-off-by: Can Guo <cang@codeaurora.org>
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -2990,6 +2990,7 @@ out_unlock:
 	ufshcd_release(hba);
 	return err;
 }
+EXPORT_SYMBOL_GPL(ufshcd_query_flag);
 
 /**
  * ufshcd_query_attr - API function for sending attribute requests
@@ -3053,6 +3054,7 @@ out_unlock:
 	ufshcd_release(hba);
 	return err;
 }
+EXPORT_SYMBOL_GPL(ufshcd_query_attr);
 
 /**
  * ufshcd_query_attr_retry() - API function for sending query
@@ -3185,6 +3187,7 @@ int ufshcd_query_descriptor_retry(struct ufs_hba *hba,
 
 	return err;
 }
+EXPORT_SYMBOL_GPL(ufshcd_query_descriptor_retry);
 
 /**
  * ufshcd_map_desc_id_to_length - map descriptor IDN to its length
